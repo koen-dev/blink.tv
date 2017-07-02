8882ff26926d86902215f868ae76687b70df493d
Koen028 <hendriks.k@ziggo.nl>
2017-07-02 23:20:35 +0200

    Added webpack for deployment

---
+++ webpack.deployment.config.js
@@ -0,0 +1,64 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "app/src/js/app.jsx"),
+  output: {
+    path: path.resolve(__dirname, "app/dist"),
+    filename: 'bundle.js',
+    publicPath: '/app/dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: "css-loader",
+            options: {
+              minimize: true
+            }
+          },{
+            loader: "sass-loader",
+            options: {
+              minimize: true
+            }
+          }],
+        })
+      },{
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: "css-loader",
+            options: {
+              minimize: true
+            }
+          }]
+        })
+      }, {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
