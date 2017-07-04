01bee368d6fbeb32aae2dd1b7e2fe0ccf79bbad8
Koen028 <hendriks.k@ziggo.nl>
2017-07-04 21:25:28 +0200

    Fix

--- app/src/js/ConnectTwitch.js
+++ app/src/js/ConnectTwitch.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 export default class ConnectTwitch extends React.Component{
   openPopup(){
-    var newWindow = window.open('http://localhost:3000/auth/twitch','name','height=600,width=450');
+    var newWindow = window.open('/auth/twitch','name','height=600,width=450');
     if (window.focus) {
       newWindow.focus();
     }
